9be2b83bf9c2e2b072022b005e35f172a6bfe177
Sanket2102 <asanket318@gmail.com>
2024-04-29 16:33:28 +0530

    removed 'tenure' from analysis dropdown

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ if st.checkbox("Enter data manually for prediction"):
 if st.checkbox("Analysis"):
 
     telecom_data = pd.read_csv("telecom.csv")
-    column = telecom_data.columns.drop(["customerID","TotalCharges","MonthlyCharges"])
+    column = telecom_data.columns.drop(["customerID","TotalCharges","MonthlyCharges","tenure"])
     col_data = st.selectbox(label = "Choose below", options=column)
     graph = plot_stacked_bar(col_data)
     st.write(graph)
